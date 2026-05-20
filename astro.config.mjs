70edc6382315b402fd9a0a0a63b099d35eb29e83
Devanshu Singh <devanshusngh@gmail.com>
2026-05-20 15:18:15 +0000

    fix: switch to server output for API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: cloudflare(),
   server: {
     host: true,
